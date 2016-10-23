327b245b5d6dc0b3728c45480d5d97e4ef82dc98
Stef Boone <stefboone@gmail.com>
2016-10-23 17:54:45 -0400

    adds css, html and js

---
+++ js/project.js
@@ -0,0 +1,10 @@
+function showThis() {
+        $("span.first").typed({
+            strings: ['Creator.','Photographer.','Maker.', 'Dog Lover.','Crafter.','Filmmaker.','Coder.', 'Dog Trainer.'],
+            backDelay: 1500,
+            typeSpeed: 100,
+            backSpeed: 100,
+            loop: true,
+        });
+    }
+    showThis()
